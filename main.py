6a68c722e82672e22d8e775ceb1076d7ef33bf2a
Rado <140254231+mitrashkov@users.noreply.github.com>
2024-07-23 08:21:52 -0700

    Fixed the chat bug

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import pickle
 import numpy as np 
 import nltk 
 from keras.models import load_model 
-from nltk.stem import WordNetLemmatizer 
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.layers import Input
+from nltk.stem import WordNetLemmatizer
+from sklearn.decomposition import PCA
 
 lemmatizer = WordNetLemmatizer() 
 intents = json.loads(open("templates/intense.json").read()) 
@@ -27,18 +32,24 @@ def bagw(sentence):
 				bag[i] = 1
 	return np.array(bag) 
 
-def predict_class(sentence): 
-	bow = bagw(sentence) 
-	res = model.predict(np.array([bow]))[0] 
-	ERROR_THRESHOLD = 0.25
-	results = [[i, r] for i, r in enumerate(res) 
-			if r > ERROR_THRESHOLD] 
-	results.sort(key=lambda x: x[1], reverse=True) 
-	return_list = [] 
-	for r in results: 
-		return_list.append({'intent': classes[r[0]], 
-							'probability': str(r[1])}) 
-		return return_list 
+def predict_class(sentence):
+    bow = bagw(sentence)
+    pca = PCA(n_components=1)  # Set n_components to None
+    bow_pca = pca.fit_transform([bow])  # Apply PCA transformation
+    res = model.predict(bow_pca)[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+model = Sequential()
+model.add(Input(shape=(1,)))  # Update the input layer to match the input shape
+model.add(Dense(27, activation='relu'))  # Add a dense layer with 27 units
+model.add(Dense(1, activation='sigmoid'))  # Add an output layer with 1 unit
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 def get_response(intents_list, intents_json): 
 	tag = intents_list[0]['intent'] 
